596d8fd0d10e68167cabf7f9ad8e673323e3e3e6
Mikhail Rusakov <rusmih98@yandex.ru>
2020-02-15 08:43:34 +0700

    еще тесты

--- src/test/java/org/loko/Transformers/DilationTest.java
+++ src/test/java/org/loko/Transformers/DilationTest.java
@@ -13,9 +13,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class DilationTest {
     private static final double factorPlus = 1.05;
     private static final double customFactor = 1.5;
+    private static final double factorMinus = 1.0 / factorPlus;
 
     @Test
-    void transform() {
+    void transformOXPlus() {
         double[][] matrix = {
                 {factorPlus, 0, 0 , 0},
                 {0, 1, 0, 0},
@@ -27,6 +28,136 @@ class DilationTest {
         LinkedList<MyPoint> myPoints = new LinkedList<>();
         myPoints.add(myPoint);
         dilation.transform(myPoints, Directions.OXPlus);
+
+        RealMatrix transformMatrix = new Array2DRowRealMatrix(matrix);
+        RealMatrix pointMatrix = new Array2DRowRealMatrix(new double[][]{{1}, {2}, {3}, {1}});
+        RealMatrix result = transformMatrix.multiply(pointMatrix);
+        double delta = 0;
+        double eps = 0.0001;
+        double[] coordinates = myPoint.getCoordinates();
+        for (int i = 0; i < 4; i++) {
+            delta += Math.abs(coordinates[i] - result.getEntry(i, 0));
+        }
+        assertTrue(delta < eps);
+    }
+
+    @Test
+    void transformOXMinus() {
+        double[][] matrix = {
+                {factorMinus, 0, 0 , 0},
+                {0, 1, 0, 0},
+                {0, 0, 1, 0},
+                {0, 0, 0, 1}
+        };
+        Dilation dilation = new Dilation();
+        MyPoint myPoint = new MyPoint(1, 2, 3);
+        LinkedList<MyPoint> myPoints = new LinkedList<>();
+        myPoints.add(myPoint);
+        dilation.transform(myPoints, Directions.OXMinus);
+
+        RealMatrix transformMatrix = new Array2DRowRealMatrix(matrix);
+        RealMatrix pointMatrix = new Array2DRowRealMatrix(new double[][]{{1}, {2}, {3}, {1}});
+        RealMatrix result = transformMatrix.multiply(pointMatrix);
+        double delta = 0;
+        double eps = 0.0001;
+        double[] coordinates = myPoint.getCoordinates();
+        for (int i = 0; i < 4; i++) {
+            delta += Math.abs(coordinates[i] - result.getEntry(i, 0));
+        }
+        assertTrue(delta < eps);
+    }
+
+    @Test
+    void transformOYPlus() {
+        double[][] matrix = {
+                {1, 0, 0 , 0},
+                {0, factorPlus, 0, 0},
+                {0, 0, 1, 0},
+                {0, 0, 0, 1}
+        };
+        Dilation dilation = new Dilation();
+        MyPoint myPoint = new MyPoint(1, 2, 3);
+        LinkedList<MyPoint> myPoints = new LinkedList<>();
+        myPoints.add(myPoint);
+        dilation.transform(myPoints, Directions.OYPlus);
+
+        RealMatrix transformMatrix = new Array2DRowRealMatrix(matrix);
+        RealMatrix pointMatrix = new Array2DRowRealMatrix(new double[][]{{1}, {2}, {3}, {1}});
+        RealMatrix result = transformMatrix.multiply(pointMatrix);
+        double delta = 0;
+        double eps = 0.0001;
+        double[] coordinates = myPoint.getCoordinates();
+        for (int i = 0; i < 4; i++) {
+            delta += Math.abs(coordinates[i] - result.getEntry(i, 0));
+        }
+        assertTrue(delta < eps);
+    }
+
+    @Test
+    void transformOYMinus() {
+        double[][] matrix = {
+                {1, 0, 0 , 0},
+                {0, factorMinus, 0, 0},
+                {0, 0, 1, 0},
+                {0, 0, 0, 1}
+        };
+        Dilation dilation = new Dilation();
+        MyPoint myPoint = new MyPoint(1, 2, 3);
+        LinkedList<MyPoint> myPoints = new LinkedList<>();
+        myPoints.add(myPoint);
+        dilation.transform(myPoints, Directions.OYMinus);
+
+        RealMatrix transformMatrix = new Array2DRowRealMatrix(matrix);
+        RealMatrix pointMatrix = new Array2DRowRealMatrix(new double[][]{{1}, {2}, {3}, {1}});
+        RealMatrix result = transformMatrix.multiply(pointMatrix);
+        double delta = 0;
+        double eps = 0.0001;
+        double[] coordinates = myPoint.getCoordinates();
+        for (int i = 0; i < 4; i++) {
+            delta += Math.abs(coordinates[i] - result.getEntry(i, 0));
+        }
+        assertTrue(delta < eps);
+    }
+
+    @Test
+    void transformOZPlus() {
+        double[][] matrix = {
+                {1, 0, 0 , 0},
+                {0, 1, 0, 0},
+                {0, 0, factorPlus, 0},
+                {0, 0, 0, 1}
+        };
+        Dilation dilation = new Dilation();
+        MyPoint myPoint = new MyPoint(1, 2, 3);
+        LinkedList<MyPoint> myPoints = new LinkedList<>();
+        myPoints.add(myPoint);
+        dilation.transform(myPoints, Directions.OZPlus);
+
+        RealMatrix transformMatrix = new Array2DRowRealMatrix(matrix);
+        RealMatrix pointMatrix = new Array2DRowRealMatrix(new double[][]{{1}, {2}, {3}, {1}});
+        RealMatrix result = transformMatrix.multiply(pointMatrix);
+        double delta = 0;
+        double eps = 0.0001;
+        double[] coordinates = myPoint.getCoordinates();
+        for (int i = 0; i < 4; i++) {
+            delta += Math.abs(coordinates[i] - result.getEntry(i, 0));
+        }
+        assertTrue(delta < eps);
+    }
+
+    @Test
+    void transformOZMinus() {
+        double[][] matrix = {
+                {1, 0, 0 , 0},
+                {0, 1, 0, 0},
+                {0, 0, factorMinus, 0},
+                {0, 0, 0, 1}
+        };
+        Dilation dilation = new Dilation();
+        MyPoint myPoint = new MyPoint(1, 2, 3);
+        LinkedList<MyPoint> myPoints = new LinkedList<>();
+        myPoints.add(myPoint);
+        dilation.transform(myPoints, Directions.OZMinus);
 
         RealMatrix transformMatrix = new Array2DRowRealMatrix(matrix);
         RealMatrix pointMatrix = new Array2DRowRealMatrix(new double[][]{{1}, {2}, {3}, {1}});
