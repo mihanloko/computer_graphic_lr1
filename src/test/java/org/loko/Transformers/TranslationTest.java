92535ec0afdcb19a93745a7a9e1c7cc650701cf3
Mikhail Rusakov <rusmih98@yandex.ru>
2020-02-15 08:50:09 +0700

    и еще тесты

--- src/test/java/org/loko/Transformers/TranslationTest.java
+++ src/test/java/org/loko/Transformers/TranslationTest.java
@@ -14,7 +14,7 @@ class TranslationTest {
     private static final int delta = 5;
 
     @Test
-    void transform() {
+    void transformOXPlus() {
         double[][] matrix = {
                 {1, 0, 0, 0},
                 {0, 1, 0, 0},
@@ -28,6 +28,141 @@ class TranslationTest {
         LinkedList<MyPoint> myPoints = new LinkedList<>();
         myPoints.add(myPoint);
         reflection.transform(myPoints, Directions.OXPlus);
+        RealMatrix transformMatrix = new Array2DRowRealMatrix(matrix);
+        RealMatrix pointMatrix = new Array2DRowRealMatrix(new double[][]{{1}, {2}, {3}, {1}});
+        RealMatrix result = transformMatrix.multiply(pointMatrix);
+        double d = 0;
+        double eps = 0.0001;
+        double[] coordinates = myPoint.getCoordinates();
+        for (int i = 0; i < 3; i++) {// при этом преобразовании вроде последняя координата не нужна. или во всех
+            d += Math.abs(coordinates[i] - result.getEntry(i, 0));
+        }
+        assertTrue(d < eps);
+    }
+
+    @Test
+    void transformOXMinus() {
+        double[][] matrix = {
+                {1, 0, 0, 0},
+                {0, 1, 0, 0},
+                {0, 0, 1, 0},
+                {0, 0, 0, 1}
+        };
+        matrix[3][0] = -delta;
+
+        Multiplier reflection = new Translation();
+        MyPoint myPoint = new MyPoint(1, 2, 3);
+        LinkedList<MyPoint> myPoints = new LinkedList<>();
+        myPoints.add(myPoint);
+        reflection.transform(myPoints, Directions.OXMinus);
+        RealMatrix transformMatrix = new Array2DRowRealMatrix(matrix);
+        RealMatrix pointMatrix = new Array2DRowRealMatrix(new double[][]{{1}, {2}, {3}, {1}});
+        RealMatrix result = transformMatrix.multiply(pointMatrix);
+        double d = 0;
+        double eps = 0.0001;
+        double[] coordinates = myPoint.getCoordinates();
+        for (int i = 0; i < 3; i++) {// при этом преобразовании вроде последняя координата не нужна. или во всех
+            d += Math.abs(coordinates[i] - result.getEntry(i, 0));
+        }
+        assertTrue(d < eps);
+    }
+
+    @Test
+    void transformOYPlus() {
+        double[][] matrix = {
+                {1, 0, 0, 0},
+                {0, 1, 0, 0},
+                {0, 0, 1, 0},
+                {0, 0, 0, 1}
+        };
+        matrix[3][1] = delta;
+
+        Multiplier reflection = new Translation();
+        MyPoint myPoint = new MyPoint(1, 2, 3);
+        LinkedList<MyPoint> myPoints = new LinkedList<>();
+        myPoints.add(myPoint);
+        reflection.transform(myPoints, Directions.OYPlus);
+        RealMatrix transformMatrix = new Array2DRowRealMatrix(matrix);
+        RealMatrix pointMatrix = new Array2DRowRealMatrix(new double[][]{{1}, {2}, {3}, {1}});
+        RealMatrix result = transformMatrix.multiply(pointMatrix);
+        double d = 0;
+        double eps = 0.0001;
+        double[] coordinates = myPoint.getCoordinates();
+        for (int i = 0; i < 3; i++) {// при этом преобразовании вроде последняя координата не нужна. или во всех
+            d += Math.abs(coordinates[i] - result.getEntry(i, 0));
+        }
+        assertTrue(d < eps);
+    }
+
+    @Test
+    void transformOYMinus() {
+        double[][] matrix = {
+                {1, 0, 0, 0},
+                {0, 1, 0, 0},
+                {0, 0, 1, 0},
+                {0, 0, 0, 1}
+        };
+        matrix[3][1] = -delta;
+
+        Multiplier reflection = new Translation();
+        MyPoint myPoint = new MyPoint(1, 2, 3);
+        LinkedList<MyPoint> myPoints = new LinkedList<>();
+        myPoints.add(myPoint);
+        reflection.transform(myPoints, Directions.OYMinus);
+        RealMatrix transformMatrix = new Array2DRowRealMatrix(matrix);
+        RealMatrix pointMatrix = new Array2DRowRealMatrix(new double[][]{{1}, {2}, {3}, {1}});
+        RealMatrix result = transformMatrix.multiply(pointMatrix);
+        double d = 0;
+        double eps = 0.0001;
+        double[] coordinates = myPoint.getCoordinates();
+        for (int i = 0; i < 3; i++) {// при этом преобразовании вроде последняя координата не нужна. или во всех
+            d += Math.abs(coordinates[i] - result.getEntry(i, 0));
+        }
+        assertTrue(d < eps);
+    }
+
+    @Test
+    void transformOZPlus() {
+        double[][] matrix = {
+                {1, 0, 0, 0},
+                {0, 1, 0, 0},
+                {0, 0, 1, 0},
+                {0, 0, 0, 1}
+        };
+        matrix[3][2] = delta;
+
+        Multiplier reflection = new Translation();
+        MyPoint myPoint = new MyPoint(1, 2, 3);
+        LinkedList<MyPoint> myPoints = new LinkedList<>();
+        myPoints.add(myPoint);
+        reflection.transform(myPoints, Directions.OZPlus);
+        RealMatrix transformMatrix = new Array2DRowRealMatrix(matrix);
+        RealMatrix pointMatrix = new Array2DRowRealMatrix(new double[][]{{1}, {2}, {3}, {1}});
+        RealMatrix result = transformMatrix.multiply(pointMatrix);
+        double d = 0;
+        double eps = 0.0001;
+        double[] coordinates = myPoint.getCoordinates();
+        for (int i = 0; i < 3; i++) {// при этом преобразовании вроде последняя координата не нужна. или во всех
+            d += Math.abs(coordinates[i] - result.getEntry(i, 0));
+        }
+        assertTrue(d < eps);
+    }
+
+    @Test
+    void transformOZMinus() {
+        double[][] matrix = {
+                {1, 0, 0, 0},
+                {0, 1, 0, 0},
+                {0, 0, 1, 0},
+                {0, 0, 0, 1}
+        };
+        matrix[3][2] = -delta;
+
+        Multiplier reflection = new Translation();
+        MyPoint myPoint = new MyPoint(1, 2, 3);
+        LinkedList<MyPoint> myPoints = new LinkedList<>();
+        myPoints.add(myPoint);
+        reflection.transform(myPoints, Directions.OZMinus);
         RealMatrix transformMatrix = new Array2DRowRealMatrix(matrix);
         RealMatrix pointMatrix = new Array2DRowRealMatrix(new double[][]{{1}, {2}, {3}, {1}});
         RealMatrix result = transformMatrix.multiply(pointMatrix);
